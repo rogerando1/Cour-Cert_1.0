8d18f31695a68e4485bbaa6b0a846e1613f5ea37
Roger R. Ando <139098175+rogerando1@users.noreply.github.com>
2023-10-14 15:02:52 +0800

    11

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,23 @@ function App() {
 
   return (
     <div className="App">
-      <h1>{message}</h1>
+      <div className ="home-banner-container">
+                <div className="home-bannerImage-container">
+                    <img src ={BackGround} alt="My BackGround" className="Bg"/>      
+                </div>
+                <div className="home-text-section">
+                    <h1 className="first-heading">
+                        Digital Solution
+                    </h1>
+                    <h1 className="second-heading">Explore Our Courses</h1>
+                    <p className="primary-text">
+                        "Education Elevated, Certification Validated"
+                    </p>
+                    <Link to="/MoreDetails" className="secondary-button">
+                        More Details <FiArrowRight/>
+                    </Link>
+                </div>
+            </div>
     </div>
   );
 }
