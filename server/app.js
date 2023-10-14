7641c1f989d63cd4b569fe715e07a1cff01646ce
Roger R. Ando <139098175+rogerando1@users.noreply.github.com>
2023-10-14 14:48:59 +0800

    111

--- server/app.js
+++ server/app.js
@@ -1,24 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-require('dotenv').config();
+const cors = require("cors");
+require("dotenv").config();
 
-//middleware
+// middleware
+const corsOptions = {
+    origin: "http://localhost:3000" // frontend URI (ReactJS)
+}
 app.use(express.json());
+app.use(cors(corsOptions));
 
-
-//connect mongodb
-mongoose.connect(process.env.MONGODB_URI).then(() =>{
-    const PORT = process.env.PORT || 8000 
+// connect MongoDB
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    const PORT = process.env.PORT || 8000
     app.listen(PORT, () => {
         console.log(`App is Listening on PORT ${PORT}`);
     })
-
-}).catch(err =>{
+}).catch(err => {
     console.log(err);
-})
+});
 
-//route
-app.get('/', (req,res) => {
-    res.status(201).json({message: 'Connected to client!'});
-})
+// route
+app.get("/", (req, res) => {
+    res.status(201).json({message: "Connected to Backend!"});
+});
